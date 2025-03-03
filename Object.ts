54938f7c59e35894b5c1e64eec9eaf4fb110885f
cupglassDEV <vincmatheo@gmail.com>
2025-03-03 12:21:51 +0700

    new version

--- Object.ts
+++ Object.ts
@@ -15,25 +15,66 @@ export function compareRef<O1 extends unknown, O2 extends unknown>(
   }
   return (Object.is(obj1, obj2));
 }
+/** Is type extended? */
+type Extended<Initial, Expected> = Initial extends Expected ? Initial : never
+
+/**
+ * Check if object was either `null` or `undefined`
+ *
+ * @param obj The object that you want to check
+ * @returns boolean
+ */
+export function isNull<
+  Expected extends null | undefined,
+  Initial extends unknown,
+>(obj: Initial): obj is Extended<Initial, Expected> {
+  if (typeof obj === "undefined") return true;
+  // null will be asserted to object
+  // see https://alexanderell.is/posts/typeof-null/
+  if (typeof obj !== "object") return false;
+  if (obj === null) return true
+  return false
+}
+
+/**
+ * Check if object was empty, and aren't null
+ *
+ * @param obj The object that you want to check
+ * @returns boolean
+ */
+export function isEmptyObject<
+  Expected extends Record<string, unknown>,
+  Initial extends unknown,
+>(obj: Initial): obj is Extended<Initial, Expected> {
+  const n = isNull(obj)
+  if (n) return false
+  if (typeof obj !== "object") return false;
+  if (Object.keys(obj as (Initial & object)).length < 1) return false;
+  return true
+}
 
 /**
  * Check if object passed was an array
  *
  * @param obj The object that you want to check
+ * @returns boolean
  */
 export function isArray<
   Expected extends ArrayLike<unknown>,
   Initial extends unknown,
->(obj: Initial): obj is Initial extends Expected ? Initial : never {
+>(obj: Initial): obj is Extended<Initial, Expected> {
+  const n = isNull(obj)
+  if (n) return false
   // or typeof Object(obj)[Symbol.iterator] === 'function'
   if (!(Symbol.iterator in Object(obj))) return false;
   return true;
 }
 
 /**
- * Check if object passed was a extended array class (for example, Int8Array, Int8ClampedArray, etc)
+ * Check if object passed was a extended array class (for example, Int8Array, Int8ClampedArray, etc) that dosent include normal Array class
  *
  * @param obj The object that you want to check
+ * @returns boolean
  */
 export function isExtendedArray<
   Expected extends Exclude<ArrayLike<unknown>, Array<unknown>>,
@@ -41,8 +82,8 @@ export function isExtendedArray<
 >(
   obj: Initial,
 ): obj is Initial extends Array<unknown> ? never
-  : Initial extends Expected ? Initial
-  : never {
+: Initial extends Expected ? Initial
+: never {
   const res = isArray<Expected, Initial>(obj);
   if (!res) return false;
   return !(obj instanceof Array);
@@ -52,42 +93,228 @@ export function isExtendedArray<
  * Check if object passed was a literal array (Not extended/custom array)
  *
  * @param obj The object that you want to check
+ * @returns boolean
  */
 export function isLiteralArray<
   Expected extends Array<unknown>,
   Initial extends unknown,
->(obj: Initial): obj is Initial extends Expected ? Initial : never {
+>(obj: Initial): obj is Extended<Initial, Expected> {
+  type Res = Extended<Initial, Expected>
   const res = isArray<Expected, Initial>(obj);
   if (!res) return false;
   return (obj instanceof Array);
 }
 
 /**
- * Accepted object for {@link newRef}
+ * Check if literal array was empty
+ *
+ * @param obj The object that you want to check
+ * @returns boolean
  */
-export type NewRefAcceptedObjects =
-  | unknown[]
-  | Record<string | number | symbol, object>
-  | boolean
-  | string
-  | number;
+export function isEmptyLiteralArray<
+  Initial extends unknown
+>(obj: Initial): boolean {
+  if (!isLiteralArray(obj)) return false
+  if (obj.length < 1) return false;
+  return true
+}
 
 /**
- * Make a new reference from object (see {@link NewRefAcceptedObjects} for the accepted objects)
+ * Check if array was empty
+ * 
+ * ℹ️ **Note**: This dosen't uses {@link isEmptyExtendedArray} nor {@link isEmptyLiteralArray}. 
+ * It only check using {@link isArray}, then get the length
+ * 
+ * @param obj The object that you want to check
+ * @returns boolean
+ */
+export function isEmptyArray<
+  Initial extends unknown
+>(obj: Initial): boolean {
+  if (!isArray(obj)) return false
+  if (obj.length < 1) return false;
+  return true
+}
+
+/**
+ * Check if literal array was empty
+ *
+ * @param obj The object that you want to check
+ * @returns boolean
+ */
+export function isEmptyExtendedArray<
+  Initial extends unknown
+>(obj: Initial): boolean {
+  if (!isExtendedArray(obj)) return false
+  if (obj.length < 1) return false;
+  return true
+}
+
+/**
+ * Check if object passed was a class
+ *
+ * @param obj The object that you want to check
+ * @returns boolean
+ */
+export function isClass<
+  Expected extends { new(...args: unknown[]): unknown },
+  Initial extends unknown,
+>(obj: Initial): obj is Extended<Initial, Expected> {
+  if (typeof obj === "undefined") return false;
+  // class is actually a function, since js picks up the constructor
+  if (typeof obj !== "function") return false;
+  const propertyNames = Object.getOwnPropertyNames(obj);
+  return (propertyNames.includes('prototype') && !propertyNames.includes('arguments'));
+}
+
+/**
+ * Make a new reference from object 
+ * 
+ * ℹ️ **Note**: `Function`, `class`, `null`, and `undefined` objects returned again as fallback
  *
  * @param obj The object that you want make a new reference from
+ * @returns New reference (or same reference (see Note)) from object
  */
-export function newRef<T extends NewRefAcceptedObjects>(obj: T): T {
-  // check if array
-  const arrayTest = isArray(obj);
-  if (arrayTest && isLiteralArray(obj)) {
+export function newRef<T>(obj: T): T {
+  // add a fallback for non-refable
+  // also, class is actually a function. Since js picks up the constructor
+  if (
+    isNull(obj)
+  ) {
+    return obj as T
+  }
+  // return new reference for object
+  if (isLiteralArray(obj)) {
     return [...obj] as T;
-  } // return new reference for object
-  else if (!arrayTest && !isExtendedArray(obj) && typeof obj === "object") {
+  } else if (typeof obj === "object") {
     return { ...obj };
-  } else {
-    // return as primitive
-    const prim = obj.valueOf();
-    return prim as T;
+  }
+  if (typeof obj === "function") {
+    return obj
+  }
+  const prim = (obj as { valueOf: () => unknown }).valueOf();
+  return prim as T;
+}
+/** Track object */
+export interface Track<Value, Max extends number|undefined=undefined> {
+  /** The value */
+  value:Value
+  /** 
+   * Listen only on changes
+   * 
+   * ℹ️ **Note**: This is different than {@link Track.observe} since it wasn't triggered on listener initialization
+   * 
+   * @param fn The function that you want to listen
+  */
+  watch(fn:(val:Value)=>void):void
+  /** 
+   * Trigger once, and listen on changes
+   * 
+   * ℹ️ **Note**: This is different than {@link Track.watch} since it trigger first on listener initialization
+   * 
+   * @param fn The function that you want to listen
+  */
+  observe(fn:(val:Value)=>void):void
+  /** 
+   * "Does this function has been added inside {@link Track}?"
+   * 
+   * @param fn The function that you want to see inside {@link Track}
+  */
+  hasFn(fn:(val:Value)=>void):boolean
+  /** Max event listeners. Can be `undefined` if dosen't set */
+  maxFn?:number
+  /** 
+   * Remove function by reference
+   * 
+   * @param fn The function that you want to remove inside {@link Track}
+  */
+  remove(fn:(val:Value)=>void):void,
+  /** Remove all function listeners */
+  removeAll():void
+}
+/**
+ * Track object
+ * 
+ * @param value The value
+ * @param maxFn (Optional) Maximum event listeners. If the listener limit has exceeded by that argument, it would be silently discarded from {@link Track.watch} and {@link Track.observe} unless you clean it
+ * @returns => {@link Track} object
+ */
+export function track<T extends unknown, Max extends number|undefined>(value:T, maxFn?:Max):Track<T, Max> {
+  let _listOfEvs : ((val:T)=>void)[] = []
+  let v = value
+  return {
+    get value(){return v},
+    set value(newV){
+      v = newV
+      if (_listOfEvs.length<1) {
+        return
+      }
+      for (let i=0;i<_listOfEvs.length;i++) {
+        _listOfEvs[i](v)
+      }
+    },
+    watch(fn){
+      if (maxFn && _listOfEvs.length>=maxFn) {
+        return
+      }
+      _listOfEvs.push(fn)
+    },
+    observe(fn){
+      if (maxFn && _listOfEvs.length>=maxFn) {
+        return
+      }
+      fn(v)
+      _listOfEvs.push(fn)
+    },
+    hasFn(fn){
+      return _listOfEvs.includes(fn)
+    },
+    remove(fn) {
+      _listOfEvs = _listOfEvs.filter((currFn)=>{
+        return compareRef(currFn, fn)
+      })
+    },
+    removeAll() {
+      _listOfEvs = []
+    }
+  }
+}
+
+// for typeof. https://stackoverflow.com/a/69655302/22147523
+const _dummyVar = typeof (1 as unknown);
+/** enum for `typeof` keyword */
+export type Typeof = typeof _dummyVar;
+
+/** enum for {@link realTypeof} */
+export type RealTypeof = "class" | "array" | "null" | Typeof
+/**
+ * Get a real `typeof`! (See docs for more info)
+ * 
+ * @param obj The object that you want make a new reference from
+ * @returns enum {@link RealTypeof}
+ */
+export function realTypeof(obj: unknown): RealTypeof {
+  const t = typeof obj
+  switch (t) {
+    case ("object"): {
+      if (obj === null) {
+        return "null"
+      } else if (Symbol.iterator in Object(obj)) {
+        return "array"
+      } else {
+        return "object"
+      }
+    }
+    case ("function"): {
+      const propertyNames = Object.getOwnPropertyNames(obj);
+      if (propertyNames.includes('prototype') && !propertyNames.includes('arguments')) {
+        return "class"
+      } else {
+        return "function"
+      }
+    }
+    default: {
+      return t;
+    }
   }
 }
--- README.md
+++ README.md
@@ -1,95 +1,134 @@
 # Object
+
 manipulating object? Nah, thats eazy!
+
+This includes type-checking at runtime, so when you compare these objects, you are very VERY safe!
+
 ## Current changelog
-BREAKING CHANGE when migrating from @cupglassdev/object. `refCompare` was renamed to `compareRef`
-## Installing 
+
+BREAKING CHANGE when migrating from @cupglassdev/object. `refCompare` was
+renamed to `compareRef`
+
+## Installing
+
 You can install this in Node! (Yes, without transpilling) By running
+
 ```shell
 $ npx jsr add @dunno/object
 ```
+
 Or in Yarn
+
 ```shell
 $ yarn dlx jsr add @dunno/object
 ```
+
 Or in pnpm
+
 ```shell
 $ pnpm dlx jsr add @dunno/object
 ```
+
 ---
+
 Or in Deno (they provide their built in JSR system, so just do this)
+
 ```shell
 $ deno add jsr:@dunno/object
 ```
+
 Or in Bun
+
 ```shell
 $ bunx jsr add @dunno/object
 ```
+
 ## Importing
+
 By default, the module dosent have a default export. You can do it like this
+
 ```js
-import * as obj from "@dunno/object"
+import * as obj from "@dunno/object";
 ```
+
 Or for [Deno](https://deno.com) users
+
 ```js
-import * as obj from "jsr:@dunno/object"
+import * as obj from "jsr:@dunno/object";
 ```
+
 ## Contributing
-If you want to report a bug, or suggestion, make a new issue under our repository (see on the 'Links' section)
+
+If you want to report a bug, or suggestion, make a new issue under our
+repository (see on the 'Links' section)
+
 ## License
+
 This package is licensed under MIT. You can see at [LICENSE](./LICENSE)
+
 ## Links
+
 - Repository https://github.com/teamdunno/object
+
 ## Example
+
 #### Compare same non-primitive reference
+
 ```js
-import * as obj from "@dunno/object"
+import * as obj from "@dunno/object";
 // This is our reference one
-const helloArray = ['Hello']
+const helloArray = ["Hello"];
 // This variable just re-forwards the reference one
-const hiArray = helloArray
+const hiArray = helloArray;
 // The output should be `true`
-console.log(obj.compareRef(helloArray, hiArray))
+console.log(obj.compareRef(helloArray, hiArray));
 ```
+
 #### Compare diffrent non-primitive reference
+
 ```js
-import * as obj from "@dunno/object"
+import * as obj from "@dunno/object";
 // This is our reference one
-const helloArray = ['Hello']
-/* 
-This is our reference two 
-(because it dosent re-forward reference one 
+const helloArray = ["Hello"];
+/*
+This is our reference two
+(because it dosent re-forward reference one
 like we do in the first example)
 */
-const hiArray = ['Hello']
+const hiArray = ["Hello"];
 /*
-The output should be `false`. 
+The output should be `false`.
 Even though the value are exactly same
 */
-console.log(obj.compareRef(helloArray, hiArray))
+console.log(obj.compareRef(helloArray, hiArray));
 ```
+
 #### Detect if array was a normal array
+
 ```js
-import * as obj from "@dunno/object"
+import * as obj from "@dunno/object";
 // make a new, normal array
-const test = ["Hello"]
+const test = ["Hello"];
 /*
  The output should be `true`,
  because its using the normal Array class
 */
-console.log(obj.isLiteralArray(test))
+console.log(obj.isLiteralArray(test));
 // The output should be `false`
-console.log(obj.isExtendedArray(test))
+console.log(obj.isExtendedArray(test));
 ```
+
 #### Detect if array was an extended array class (Int16Array, or such)
+
 ```js
-import * as obj from "@dunno/object"
+import * as obj from "@dunno/object";
 // make a new Int16Array
-const test = new Int16Array([8])
+const test = new Int16Array([8]);
 // The output should be `false`
-console.log(obj.isLiteralArray(test))
+console.log(obj.isLiteralArray(test));
 /*
- The output should be `true`, 
+ The output should be `true`,
  because its not a standard array class
 */
-console.log(obj.isExtendedArray(test))
+console.log(obj.isExtendedArray(test));
 ```
